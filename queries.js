49b9f077612a1fe3e55217e2c63671e874524898
thextool <matthew.riach@gmail.com>
2018-08-02 09:42:15 -0600

    :zap: created routes and queries and ready to submit tested in postman

--- queries.js
+++ queries.js
@@ -2,13 +2,30 @@ const db = require("./db/database-connection");
 
 module.exports = {
     list(){
+        return db('game')
     },
     read(id){
+        return db('game')
+        .where('id', id)
     },
     create(game){
+        return db('game')
+        .insert(game)
+        .returning('*')
+        .then(record => record[0])
     },
     update(id, game){
+        return db('game')
+        .update(game)
+        .where('id', id)
+        .returning('*')
+        .then(record => record[0])
     },
     delete(id){
+        return db('game')
+        .delete()
+        .where('id', id)
+        .returning('*')
+        .then(record => record[0])
     }
 };
